42cd71b63667b28fc2aacf46c150116628d0b697
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-10-27 19:47:18 +0200

    Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+
+class App extends Component {
+  fetchImages = async () => {
+    try {
+      const response = await fetch(
+        'https://pixabay.com/api/?q=cat&page=1&key=39482556-d60da0ad7dc5ab6f886d79ae4&image_type=photo&orientation=horizontal&per_page=12'
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Błąd: ', error);
+    }
+  };
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  render() {
+    return <Searchbar />;
+  }
+}
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import styles from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <header className={styles.searchbar}>
+        <form className={styles.searchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={styles.searchFormButton}>
+            <span className={styles.searchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={styles.searchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
